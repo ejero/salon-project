36eb618cce6991d2ee1f8f23db1ba4e794c298b5
ejero <rositaemakpo@gmail.com>
2022-12-08 13:07:50 -0800

    Adds style to heading

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,14 @@ import img from '../public/images/stylist/kinkyroots.png'
 import Link from 'next/link';
 
 
-
-
 export default function Home() {  
   function WelcomeMessage(){
     return (
       <>
       <div className={styles.nameWrapper}>
         <h1 className={styles.message} >The <span class={styles.best}>best</span> natural hair salon expience in town</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        <p className={styles.btn}><Link href="../appointments/appointments">Book an appointment</Link></p>
+        <p className={styles.para}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+        <p className={styles.btn}><Link href="../appointments/appointments">Request an appointment</Link></p>
       </div>
       </>
     )
@@ -32,8 +30,6 @@ export default function Home() {
       </>
     )
   }
-
-
 
 
   function Footer(){
