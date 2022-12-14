71ce3d0aeb53dc0d959af4b0f9acd927e629f7d2
ejero <rositaemakpo@gmail.com>
2022-12-14 15:05:35 -0800

    Adds new footer and services

--- pages/footer/footer.js
+++ pages/footer/footer.js
@@ -1,10 +1,49 @@
 import styles from './Footer.module.css'
+import Image from 'next/image'
+import logo from '../../public/images/logo/kr-logo6.png'
+import Link from 'next/link'
+import twitter from './twitter.png'
+import github from './Github-mark-white.png'
+import ln from './In-White-96.png'
+import mail from './mail2.svg'
+
 
 export default function Footer(){
 return(
     <>
     <footer>
-    <p className={styles.footer}>Hi</p>
+    <div className={styles.footer}>
+        <div className={styles.icons}>
+
+        <Link href="https://twitter.com/lovelittlerose" target="_blank" >
+            <Image className={styles.iconSvg} src={twitter} alt="Twitter logo"/>
+        </Link> 
+            
+        <Link href="https://github.com/ejero" target="_blank" >
+            <Image  className={styles.iconSvg} src={github} alt="GitHub logo"/>
+        </Link>
+
+        <Link href="https://www.linkedin.com/in/rosita-emakpo/" target="_blank" >  
+        <Image className={styles.iconSvg} src={ln} alt="LinkedIn logo"/>
+
+        </Link>    
+            <Link href="mailto:rosita@rosita.tech" target="_blank">
+            <Image className={styles.iconSvg} src={mail} alt="email icon" />
+        </Link>
+        
+        </div>
+        
+        <Image src={logo} 
+            height={50}
+            width={200}
+        />
+        <p className={styles.rights}>&#169;2022 Kinky roots, LLC, All rights lorem ipsum ullamco laboris nisi </p>
+    </div>
+
+
+
+
+
     </footer>
     </>
 )
--- pages/salon-services/services.js
+++ pages/salon-services/services.js
@@ -1,7 +1,81 @@
-import NavBar from '../nav-bar/nav-bar'
+import styles from './Services.module.css'
+import Image from 'next/image'
+import pic1 from '../../public/images/haircut.jpg'
+import pic2 from '../../public/images/hair-color.jpg'
+import pic3 from '../../public/images/prof_lady.jpg'
 
 export default function Services(){
     return (<>
-    <h1>Services</h1>  
+        <div className={styles.outter}>
+
+        
+        <div className={styles.firstContainer}>
+            <Image className={styles.img} src={pic1}
+                    width={400}
+                    height={550}
+                />
+
+            <div className={`${styles.img} ${styles.right}`}>
+                <p className={styles.banner}>Why choose us</p>
+                <h1 className={styles.welcome}>Exceptional services at each visit for all curl types</h1>
+                <p className={styles.comment}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. oluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat </p>
+
+
+                <div className={styles.innerContainer}>
+                    <p className={styles.item}>patient</p>
+                    <p className={styles.item} >consistent</p>
+                    <p className={styles.item}>licenced</p>
+                    <p className={styles.item}>insured</p>
+                </div>
+            </div>
+        </div>
+
+        <div className={styles.firstContainer}>
+            
+            <div className={`${styles.img} ${styles.right}`}>
+                <p className={styles.banner}>Services</p>
+                <h1 className={styles.welcome}>Let us style your wonderful curls</h1>
+                <p className={styles.comment}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. oluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat </p>
+
+
+                <div className={styles.innerContainer}>
+                    <p className={styles.item}>scalp analysis</p>
+                    <p className={styles.item}>consultations</p>
+                    <p className={styles.item}>Hair Transplant</p>
+                    <p className={styles.item}>Braids</p>
+                </div>
+            </div>
+
+            <Image className={styles.img} src={pic2}
+                    width={400}
+                    height={550}
+                />
+        </div>
+
+
+
+        <div className={styles.firstContainer}>
+            <Image className={styles.img} src={pic3}
+                    width={400}
+                    height={550}
+                />
+
+            <div className={`${styles.img} ${styles.right}`}>
+                <p className={styles.banner}>First time clients get 10% off</p>
+                <h1 className={styles.welcome}>Exceptional services at each visit for all curl types</h1>
+                <p className={styles.comment}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. oluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat </p>
+
+                <div className={styles.innerContainer}>
+                    <p className={styles.item}>Weddings</p>
+                    <p className={styles.item}>Patries</p>
+                    <p className={styles.item}>School events</p>
+                    <p className={styles.item}>Anniversaries</p>
+                </div>
+            </div>
+            </div>
+
+
+        </div>
     </> )
 }
+
